94ef7c38b4884cf2bf05a26f7ab4138287de25ea
evgvb <boevev@yandex.ru>
2026-02-13 11:48:37 +0800

    ФЗ-14

--- src/test/java/ru/practicum/shareit/UserTest.java
+++ src/test/java/ru/practicum/shareit/UserTest.java
@@ -34,26 +34,6 @@ public class UserTest {
         assertEquals("user1@email.com", savedUser.getEmail(), "Email должен сохраниться");
     }
 
-    // проверка уникальности email при сохранении
-    @Test
-    void save_shouldThrowExceptionWhenEmailExists() {
-        User user1 = User.builder()
-                .name("User 1")
-                .email("user1@email.com")
-                .build();
-        userRepository.save(user1);
-
-        // Создаем второго пользователя с таким же email
-        User user2 = User.builder()
-                .name("User 2")
-                .email("user1@email.com")
-                .build();
-
-        assertThrows(IllegalArgumentException.class,
-                () -> userRepository.save(user2),
-                "Должно быть выброшено исключение при попытке сохранить пользователя с существующим email");
-    }
-
     // поиска пользователя по ID
     @Test
     void findById_shouldReturnUserWhenExists() {
