a703f7fc0415655d3b8e96aab30b4470b5b1a0af
evgvb <boevev@yandex.ru>
2026-03-12 14:40:29 +0800

    ФЗ-16

--- gateway/src/main/java/ru/practicum/shareit/client/properties/ShareItServerProperties.java
+++ gateway/src/main/java/ru/practicum/shareit/client/properties/ShareItServerProperties.java
@@ -1,10 +1,8 @@
 package ru.practicum.shareit.client.properties;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@ConfigurationProperties(prefix = "shareit-server")
 @RequiredArgsConstructor
 @Getter
 public class ShareItServerProperties {
